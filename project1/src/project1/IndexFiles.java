cafcb8014d699f7041442720cea4dc3bf5e99b51
Yiwei Gao <ygao93@gmail.com>
2016-02-09 21:06:30 -0500

    main logic in place

--- project1/src/project1/IndexFiles.java
+++ project1/src/project1/IndexFiles.java
@@ -50,7 +50,7 @@ import java.util.Date;
  * Run it with no command-line arguments for usage information.
  */
 public class IndexFiles {
-  
+
   private IndexFiles() {}
 
   /** Index all text files under a directory. */
@@ -84,7 +84,7 @@ public class IndexFiles {
       System.out.println("Document directory '" +docDir.toAbsolutePath()+ "' does not exist or is not readable, please check the path");
       System.exit(1);
     }
-    
+
     Date start = new Date();
     try {
       System.out.println("Indexing to directory '" + indexPath + "'...");
@@ -134,14 +134,14 @@ public class IndexFiles {
   /**
    * Indexes the given file using the given writer, or if a directory is given,
    * recurses over files and directories found under the given directory.
-   * 
+   *
    * NOTE: This method indexes one document per input file.  This is slow.  For good
    * throughput, put multiple documents into your input file(s).  An example of this is
    * in the benchmark module, which can create "line doc" files, one document per line,
    * using the
    * <a href="../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
    * >WriteLineDocTask</a>.
-   *  
+   *
    * @param writer Writer to the index where the given file/dir info will be stored
    * @param path The file to index, or the directory to recurse into to find files to index
    * @throws IOException If there is a low-level I/O error
@@ -167,42 +167,45 @@ public class IndexFiles {
   /** Indexes a single document */
   static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
     try (InputStream stream = Files.newInputStream(file)) {
-      // make a new, empty document
-      Document doc = new Document();
-      
-      // Add the path of the file as a field named "path".  Use a
-      // field that is indexed (i.e. searchable), but don't tokenize 
-      // the field into separate words and don't index term frequency
-      // or positional information:
-      Field pathField = new StringField("path", file.toString(), Field.Store.YES);
-      doc.add(pathField);
-      
-      // Add the last modified date of the file a field named "modified".
-      // Use a LongField that is indexed (i.e. efficiently filterable with
-      // NumericRangeFilter).  This indexes to milli-second resolution, which
-      // is often too fine.  You could instead create a number based on
-      // year/month/day/hour/minutes/seconds, down the resolution you require.
-      // For example the long value 2011021714 would mean
-      // February 17, 2011, 2-3 PM.
-      doc.add(new LongField("modified", lastModified, Field.Store.NO));
-      
-      // Add the contents of the file to a field named "contents".  Specify a Reader,
-      // so that the text of the file is tokenized and indexed, but not stored.
-      // Note that FileReader expects the file to be in UTF-8 encoding.
-      // If that's not the case searching for special characters will fail.
-      doc.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
-      
-      if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-        // New index, so we just add the document (no old document can be there):
-        System.out.println("adding " + file);
-        writer.addDocument(doc);
-      } else {
-        // Existing index (an old copy of this document may have been indexed) so 
-        // we use updateDocument instead to replace the old one matching the exact 
-        // path, if present:
-        System.out.println("updating " + file);
-        writer.updateDocument(new Term("path", file.toString()), doc);
-      }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+        String docBeginTag = "<DOC>";
+        String docEndTag = "</DOC";
+        boolean inDoc = false;
+        StringBuilder stringBuilder = new StringBuilder();
+        Document document = new Document();
+        document.add(new StringField("path", file.toString(), Field.Store.YES));
+        document.add(new LongField("modified", lastModified, Field.Store.NO));
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.equals(docBeginTag)) {
+                inDoc = true;
+                continue;
+            } else if (line.equals(docEndTag)) {
+                inDoc = false;
+                document.add(new TextField("contents", stringBuilder.toString(), Field.Store.NO));
+
+                if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+                    // New index, so we just add the document (no old document can be there):
+                    System.out.println("adding " + file);
+                    writer.addDocument(document);
+                } else {
+                    // Existing index (an old copy of this document may have been indexed) so
+                    // we use updateDocument instead to replace the old one matching the exact
+                    // path, if present:
+                    System.out.println("updating " + file);
+                    writer.updateDocument(new Term("path", file.toString()), document);
+                }
+
+                document = new Document();
+                stringBuilder = new StringBuilder();
+
+                continue;
+            }
+            if (inDoc) {
+                stringBuilder.append(line).append("\n");
+            }
+        }
     }
   }
 }
