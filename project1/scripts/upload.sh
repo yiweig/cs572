cbb7cc8b46773d53549936004466af877d87dd24
Yiwei Gao <ygao93@gmail.com>
2016-02-09 20:00:50 -0500

    better script

--- project1/scripts/upload.sh
+++ project1/scripts/upload.sh
@@ -1,1 +1,4 @@
-scp ../out/artifacts/project1_jar/project1.jar lab:/aut/proj/cs572/ygao56/project1
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "scping jar..."
+scp ${DIR}/../out/artifacts/project1_jar/project1.jar lab:/aut/proj/cs572/ygao56/project1
